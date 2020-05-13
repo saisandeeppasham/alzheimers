6b32a3e7340b432abb22cc63f078949cfdaf7ab8
balupeddireddy08 <59537760+balupeddireddy08@users.noreply.github.com>
2020-05-13 18:28:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
 
     return render_template('index.html', prediction_text='Your Result: '+result)
 if __name__ == '__main__':
-    app.run(port = 5000, debug=False)
+    app.run(port = 5000, debug=True)
