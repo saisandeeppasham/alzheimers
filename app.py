ad55b101a2a3a2c7c4593595d4bf56ca9689865c
balupeddireddy08 <59537760+balupeddireddy08@users.noreply.github.com>
2020-05-15 10:15:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as numpy
 app = Flask(__name__,template_folder='templates')
-model = pickle.load(open('model_gb.pkl', 'rb'))
+model_gb = pickle.load(open('model_gb.pkl', 'rb'))
+model_ada = pickle.load(open('model_ada.pkl', 'rb'))
+model_et = pickle.load(open('model_et.pkl', 'rb'))
+model_rf = pickle.load(open('model_rf.pkl', 'rb'))
+model_svc = pickle.load(open('model_svc.pkl', 'rb'))
+#model_xgb = pickle.load(open('model_xgb.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -32,15 +37,23 @@ def predict():
     
     inputs=[[i1,i2,i3,i4,i5,i6,i7,i8]]
     
+    x1=model_gb.predict(inputs)[0]
+    x2=model_ada.predict(inputs)[0]
+    x3=model_et.predict(inputs)[0]
+    x4=model_rf.predict(inputs)[0]
+    x5=model_svc.predict(inputs)[0]
+    #x6=model_xgb.predict(inputs)[0]
+    c=[x1,x2,x3,x4,x5]
+    
+    
     #inputs = [np.array(inputs)]
     #result=str(model.predict(inputs))
     #print(model.predict(inputs))
-    if  model.predict(inputs)[0]==1:
+    if  c.count(1)>c.count(0):
         result='Demented'
-        
     else:
         result='Non-Demented'
 
     return render_template('index.html', prediction_text='Your Result: '+result)
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
+    app.run(port = 5000, debug=False)
